664f311fbb08d26630f97bc3a883e763a2381b1d
Marcos Gruber <marcosgruber7@gmail.com>
2025-11-19 12:18:46 -0300

    Mejoras de finaciacion.html + estilos

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 import 'dotenv/config';
 
-// Pequeña función para validar variables obligatorias
 const required = (key, optional = false) => {
   const value = process.env[key];
   if (!value && !optional) {
@@ -10,33 +9,27 @@ const required = (key, optional = false) => {
 };
 
 export const config = {
-  // Entorno general
   env: process.env.NODE_ENV || 'development',
   port: parseInt(process.env.PORT || '3000', 10),
 
-  // Base de datos
   dbUrl: required('DATABASE_URL'),
 
-  // Configuración general de WhatsApp
   whatsapp: {
-    provider: (process.env.WHATSAPP_PROVIDER || 'twilio').toLowerCase(), // 'twilio' o 'meta'
-    dryRun: String(process.env.WHATSAPP_DRY_RUN || 'true').toLowerCase() === 'true',
+    provider: (process.env.WHATSAPP_PROVIDER || 'meta').toLowerCase(),
+    dryRun: String(process.env.WHATSAPP_DRY_RUN || 'false').toLowerCase() === 'true',
   },
 
-  // Configuración de Twilio
+  // WhatsApp Cloud API
+  whatsappPhoneId: "897550086768330",
+  whatsappToken: "EAAUyO75ogaQBPZBghndGnyZAV4v1DgLsgIJIHCK10VABHpZA6vQ70j2ryX317PykzZADgGktxe2gHWpMaH0BdZCQCVmXwGZChZC2ZAuJKU5PqUHFIU2dnt2kkk60y0YvhzjuPeZCZAcZAklGvIHRMHh5p8iNQ5atB3N7cpfpfKu191w2ZCodQYHo4bD0OoGO1aZBVNbIbvCrJy0sJ7CxODPv5KIiVAONwb8AMldl6zE4zzXLBax7kgsiPBa75FSw8kZAeDjpu78xrAMs6QkHHmjZAfbgttZC3wuIHHZBxblSeLfI2jkQZD",
+
+  // Twilio (no usado si whatsapp.provider = meta)
   twilio: {
-    sid: required('TWILIO_ACCOUNT_SID'),
-    token: required('TWILIO_AUTH_TOKEN'),
-    from: required('TWILIO_WHATSAPP_FROM'),
+    sid: process.env.TWILIO_ACCOUNT_SID,
+    token: process.env.TWILIO_AUTH_TOKEN,
+    from: process.env.TWILIO_WHATSAPP_FROM,
   },
 
-  // Whatsaap
-  whatsappPhoneId: 897550086768330,   // ID del número
-  whatsappToken: EAAUyO75ogaQBPyNM6jlgOJnvhODaVJrZAnZCTb2ER5aqHZB8UfYmsS99dwtlzrZCmBru0UoXWCYaZCiMOXAEDy7izlbJQJW3ugqQDaKJPFUcMSMXb9X6D42zBfJz3vL0ZCngkVOVRhULETtfM5ErFNiQXY1khvAVwpUHDdPi1ehHtMGaXNJRpl116bVuHuUQkQCq85wskfki6tpKpXZB0JcuTYfFJ8PmJVZCv16LW2KZAy86ZBubZCOL8pD9CLVMJ6d6BSb6TWTcUhPkRX4k89OKLijHqTdJdLmwdLCsEJFDAZDZD,        // TOKEN (Identificador de acceso)
-
-  // Enlace a Mercado Pago
   mpLink: required('MP_LINK'),
-
-  // Clave de cifrado local (si la usás)
   encryptionKey: required('ENCRYPTION_KEY'),
 };
